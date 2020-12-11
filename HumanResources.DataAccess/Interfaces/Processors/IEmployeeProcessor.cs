dec391123bb96b2b7b1bc6ab83e122b724ae3a00
Krzysztof Pajak <krzysztof.pajak.leamington@gmail.com>
2020-12-11 10:38:27 +0000

    Added EmployeeProcessor

---
+++ HumanResources.DataAccess/Interfaces/Processors/IEmployeeProcessor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanResources.DataAccess.Interfaces.Processors
+{
+    public interface IEmployeeProcessor
+    {
+    }
+}
