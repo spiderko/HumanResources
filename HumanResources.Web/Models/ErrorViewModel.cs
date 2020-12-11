442219209a6378a734df6c4ed398caf1d9940f5f
Krzysztof Pajak <krzysztof.pajak.leamington@gmail.com>
2020-12-11 10:14:32 +0000

    Add project files.

---
+++ HumanResources.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HumanResources.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
