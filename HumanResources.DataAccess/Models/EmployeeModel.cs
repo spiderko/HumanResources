7daea23d947c22629648cf02d3bde8554df7dc5d
Krzysztof Pajak <krzysztof.pajak.leamington@gmail.com>
2020-12-11 10:33:32 +0000

    Added EmployeeModel

---
+++ HumanResources.DataAccess/Models/EmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanResources.DataAccess.Models
+{
+    public class EmployeeModel : BaseModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
