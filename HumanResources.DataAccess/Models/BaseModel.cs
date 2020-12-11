4eb418b4cc7eed73a4d136d527623842738e0b68
Krzysztof Pajak <krzysztof.pajak.leamington@gmail.com>
2020-12-11 10:28:57 +0000

    Added Dapper, SqlClient and Base Model

---
+++ HumanResources.DataAccess/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HumanResources.DataAccess.Models
+{
+    public class BaseModel
+    {
+        public Guid uid { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+        public bool Deleted { get; set; }
+
+    }
+}
